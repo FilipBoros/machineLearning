b5d51312c60e719f581702c9749bde60f6a7e3e1
Filip Boros <boros.filip@gmail.com>
2020-05-25 16:20:05 +0200

    Update __init__.py

--- custom_gym/envs/__init__.py
+++ custom_gym/envs/__init__.py
@@ -1,5 +1,18 @@
-from gym.envs.registration import register
+import gym
 
-register(id='SkiingGame-v0',
-	entry_point='envs.custom_env_dir:SkiingGame'
+def register(id, entry_point, force=True):
+    env_specs = gym.envs.registry.env_specs
+    if id in env_specs.keys():
+        if not force:
+            return
+        del env_specs[id]
+    gym.register(
+        id=id,
+        entry_point=entry_point,
+    )
+
+# Register modified versions of existing environments
+register(
+    id='SkiingGame-v0',
+    entry_point='envs.custom_env_dir:SkiingGame',
 )
